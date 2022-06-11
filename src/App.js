bf3a8837897822a763943aad7dc2615f447016ea
Yeongmin_Chae <codudals98@naver.com>
2022-06-11 19:52:02 +0900

    coin tracer (unsuccess) 11.june

--- src/App.js
+++ src/App.js
@@ -1,40 +1,71 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo == "") {
-      return;
-    }
-    // setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDos((currentArray) => [...currentArray, toDo]); // current list apears first and a new element will add at the last of array
-    setToDo("");
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [amount, setAmount] = useState();
+  const [inverted, setInverted] = useState(false);
+  const [index, setIndex] = useState();
+  const reset = () => setAmount(0);
+  const onChange = (event) => {
+    setAmount(event.target.value);
   };
-  console.log(toDos);
+  const flipped = () => {
+    setInverted((current) => !inverted);
+    reset();
+  };
+  const onSelect = (event) => {
+    return setIndex(event.target.value);
+  };
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers?limit=10")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My ToDos ({toDos.length}) </h1>
-      <form onSubmit={onSubmit}>
+      <h1>The Coins! {loading ? "" : `(${coins.length})`} </h1>
+      <label>Choose the coins : </label>
+      {loading ? (
+        <strong>Loading...</strong>
+      ) : (
+        <select onChange={onSelect} value={index}>
+          {coins.map((coin) => (
+            <option key={coin.id}>
+              {coin.name} ({coin.symbol}: ${coin.quotes.USD.price} USD)
+            </option>
+          ))}
+        </select>
+      )}
+      <hr />
+      <div>
+        <label>USD($) : </label>
         <input
           onChange={onChange}
-          value={toDo}
-          type="text"
-          placeholder="write your todos"
+          value={inverted ? amount * 29269.9871 : amount}
+          placeholder="Amount of USD$"
+          type="number"
+          disabled={inverted}
         />
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
-      {/* map allows me to transform the items in a array into whatever i want 
-      and it will ruturn with new array 
-      [a,b,c,d].map((item) => item.toUpperCase()) = [A,B,C,D] */}
+      </div>
+      <div>
+        <label>
+          {/* {coins.map((coin) => ())} */}
+          Bitcoin
+        </label>
+        <input
+          onChange={onChange}
+          value={inverted ? amount : amount / 29269.9871}
+          placeholder="Amount of Coin"
+          type="number"
+          disabled={!inverted}
+        />
+      </div>
+      <button onClick={reset}>Reset</button>
+      <button onClick={flipped}>{inverted ? "Turn Back" : "inverted"}</button>
     </div>
   );
 }
