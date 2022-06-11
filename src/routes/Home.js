a64b393f706adc3a1513bffd786e467d3ead0a13
Yeongmin_Chae <codudals98@naver.com>
2022-06-11 23:03:34 +0900

    movie app part3 (React Router)

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Movie from "./Movie";
+import Movie from "../components/Movie";
 
 function Home() {
   const [loading, setLoading] = useState(true);
