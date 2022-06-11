a99d23c9eb91946d2a96970173e13e5f999122de
Yeongmin_Chae <codudals98@naver.com>
2022-06-11 10:55:02 +0900

    toDos part1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  return <div></div>;
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo == "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    // add examples in list ->
+    // li=[1,2,3,4]
+    // [6,li] -> return [6, array(4)]
+    // [6,...li] -> return [6,1,2,3,4)]
+  };
+  console.log(toDos);
+  return (
+    <div>
+      <h1>My ToDos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="write your todos"
+        />
+        <button>Add To Do</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
